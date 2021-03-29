7dc894dff6017c042e5e9ad24d378c10394dc708
Jesse1204 <ziheng.cao@nyu.edu>
2021-03-29 15:36:00 -0400

    Update rule.py

--- rule/rule.py
+++ rule/rule.py
@@ -1,8 +1,12 @@
 import re
+from engine.sast import Info
 
 class Rule:
 
     def __init__(self, description, title, recommendation):
         self.description = description
-        self.title = title
         self.recommendation = recommendation
+    
+def Match(self, rule, inputFile):
+    info = Info(rule.description, rule.recommendation)
+    
