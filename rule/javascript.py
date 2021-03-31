a1e8a9e2d2caff3626fad7ea6139e9101e561c6e
Jesse1204 <zc1717@nyu.edu>
2021-03-30 22:03:32 -0400

    Update recommendation

--- rule/javascript.py
+++ rule/javascript.py
@@ -3,7 +3,7 @@ import re
 
 JavascriptRules = [
     Rule('Passing untreated parameters to queries in the database can cause an SQL injection, or even a NoSQL query injection.',
-        '789',
+        '',
         ExactMatch = re.compile(r'\.(find|drop|create|explain|delete|count|bulk|copy).*\n*{.*\n*\$where(?:\'|"|):.*(?:req\.|req\.query|req\.body|req\.param)')),
     Rule('Test, remove it later',
          'test',
